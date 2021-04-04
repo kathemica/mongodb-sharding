ddf5548e4f7902069822f4aae854475d89354f63
kathe <kathemica@gmail.com>
2021-04-04 16:10:36 -0300

    Updating scripts

--- config.sh
+++ config.sh
@@ -14,49 +14,68 @@ printf "\n"
 #---------------------------------------------------------------
 #Config server
 #---------------------------------------------------------------
-printf '\e[1;32m%-6s\e[m' "Consig server: creando contenedores en Docker\n"
+printf '\e[1;32m%-6s\e[m' "Config server: creando contenedores en Docker"
+printf "\n"
 #Creamos los contenedores
 docker-compose -f docker-config/docker-compose-config-server.yaml up -d
 
+sleep 5
+
 printf "\n"
-printf '\e[1;32m%-6s\e[m' "Consig server: configurando el replicaset\n"
+printf '\e[1;32m%-6s\e[m' "Config server: configurando el replicaset"
+printf "\n"
 docker exec config-srv01 sh -c "mongo < ./scripts/config-server.js"
 
 #---------------------------------------------------------------
 #Nodo 01
 #---------------------------------------------------------------
-printf '\e[1;32m%-6s\e[m' "Nodo 01: creando contenedores en Docker\n"
+printf '\e[1;32m%-6s\e[m' "Nodo 01: creando contenedores en Docker"
+printf "\n"
 #Creamos los contenedores
 docker-compose -f docker-config/docker-compose-nodo01.yaml up -d
 
+sleep 5
+
 printf "\n"
-printf '\e[1;32m%-6s\e[m' "Nodo 01: configurando el replicaset\n"
+printf '\e[1;32m%-6s\e[m' "Nodo 01: configurando el replicaset"
+printf "\n"
 docker exec nodo-shard01-srv01 sh -c "mongo < ./scripts/config-nodo01.js"
 
 #---------------------------------------------------------------
 #Nodo 02
 #---------------------------------------------------------------
-printf '\e[1;32m%-6s\e[m' "Nodo 02: creando contenedores en Docker\n"
+printf "\n"
+printf '\e[1;32m%-6s\e[m' "Nodo 02: creando contenedores en Docker"
+printf "\n"
 #Creamos los contenedores
 docker-compose -f docker-config/docker-compose-nodo02.yaml up -d
 
+sleep 5
+
 printf "\n"
-printf '\e[1;32m%-6s\e[m' "Nodo 02: configurando el replicaset\n"
+printf '\e[1;32m%-6s\e[m' "Nodo 02: configurando el replicaset"
+printf "\n"
 docker exec nodo-shard02-srv01 sh -c "mongo < ./scripts/config-nodo02.js"
 
 #---------------------------------------------------------------
 #Mongos
 #---------------------------------------------------------------
-printf '\e[1;32m%-6s\e[m' "Mongos: creando contenedores en Docker\n"
+printf "\n"
+printf '\e[1;32m%-6s\e[m' "Mongos: creando contenedores en Docker"
+printf "\n"
 #Creamos los contenedores
 docker-compose -f docker-config/docker-compose-mongos.yaml up -d
 
+sleep 5
+
 printf "\n"
-printf '\e[1;32m%-6s\e[m' "Mongos: configurando mongos\n"
+printf '\e[1;32m%-6s\e[m' "Mongos: configurando mongos"
+printf "\n"
 docker exec mongos sh -c "mongo < ./scripts/config-mongos.js"
 
 printf "\n"
-printf '\e[1;32m%-6s\e[m' "Mongos: configurando mongos - Añadiendo Nodo01 y Nodo02\n"
+printf '\e[1;32m%-6s\e[m' "Mongos: configurando mongos - Añadiendo Nodo01 y Nodo02"
+printf "\n"
 docker exec mongos sh -c "mongo < ./scripts/config-mongos.js"
 
 
