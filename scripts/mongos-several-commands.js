4b6894dc80b9926e4df375361774828e6c8dfeaf
kathe <kathemica@gmail.com>
2021-04-05 22:36:23 -0300

    Script update

--- scripts/mongos-several-commands.js
+++ scripts/mongos-several-commands.js
@@ -1,4 +1,5 @@
 use finanzas
+db.createCollection('facturas')
 db.facturas.createIndex({"cliente.cuit" : "hashed"})
 sh.shardCollection("finanzas.facturas", {"cliente.cuit" : "hashed"})
 sh.status()
