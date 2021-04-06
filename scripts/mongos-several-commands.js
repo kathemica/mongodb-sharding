6e8dbb5e98e361ed496439dd6661e410df881620
kathe <kathemica@gmail.com>
2021-04-05 22:44:44 -0300

    Script update: still not sharding.. pretty close

--- scripts/mongos-several-commands.js
+++ scripts/mongos-several-commands.js
@@ -1,6 +1,7 @@
 use finanzas
 db.createCollection('facturas')
 db.facturas.createIndex({"cliente.cuit" : "hashed"})
+sh.enableSharding("finanzas")
 sh.shardCollection("finanzas.facturas", {"cliente.cuit" : "hashed"})
 sh.status()
 db.facturas.getShardDistribution()
